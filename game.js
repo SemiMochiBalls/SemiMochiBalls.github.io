529f819c1684217a1d33b1d63e7f20971dd98168
Sachi Reyes <123606903+SemiMochiBalls@users.noreply.github.com>
2023-07-12 22:34:11 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,24 +1,24 @@
 // Get the canvas element
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-//FIREFOX EDGE OK CHROME NO BUG BUG
+
 // Game variables
 let score = 0;
-let hoppingInterval = 900; // Initial hopping interval in milliseconds
+let hoppingInterval = 1500; // Initial hopping interval in milliseconds
 let bugX, bugY;
 let bugSize = 64; // Size of the bug image
 
 // Bug image
 const bugImage = new Image();
-bugImage.src = "YnVnCg==.png";
+bugImage.src = "bug.png"; // Replace "bug.png" with your bug image file
 
 // Splattered bug image
 const splatteredImage = new Image();
-splatteredImage.src = "c3BsYXR0ZXJlZA==.png";
+splatteredImage.src = "splattered.png"; // Replace "splattered.png" with your splattered bug image file
 
 // Play zone image
 const playZoneImage = new Image();
-playZoneImage.src = "cGxheXpvbmU=.png";
+playZoneImage.src = "playzone.png"; // Replace "playzone.png" with your play zone image file
 
 // Score display
 const scoreDisplay = document.getElementById("scoreDisplay");
@@ -36,16 +36,12 @@ function updateBugPosition() {
 
 // Draw the bug on the canvas
 function drawBug() {
-    // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // Draw the play zone image
     ctx.drawImage(playZoneImage, 0, 0, canvas.width, canvas.height);
 
     if (!isSplattered) {
-        // Draw the bug image if it hasn't been splattered yet
         ctx.drawImage(bugImage, bugX, bugY, bugSize, bugSize);
     } else {
-        // Draw the splattered image if it has been splattered
         ctx.drawImage(splatteredImage, bugX, bugY, bugSize, bugSize);
     }
 }
@@ -55,34 +51,33 @@ function updateScoreDisplay() {
     scoreDisplay.textContent = "Score: " + score;
 }
 
-    function handleClick(event) {
-        // Get the mouse coordinates relative to the canvas
-        const mouseX = event.clientX - canvas.offsetLeft;
-        const mouseY = event.clientY - canvas.offsetTop;
+// Handle mouse click events
+function handleClick(event) {
+    const mouseX = event.clientX - canvas.offsetLeft;
+    const mouseY = event.clientY - canvas.offsetTop;
 
-        // Check if the bug has been clicked
-        if (
-            !isSplattered &&
-            mouseX >= bugX - 25 &&
-            mouseX <= bugX + bugSize + 25 &&
-            mouseY >= bugY - 25 &&
-            mouseY <= bugY + bugSize + 25
-        ) {
-            // Increase the score and speed up the bug hopping
-            score++;
-            hoppingInterval -= 100;
-            isSplattered = true;
-            updateScoreDisplay();
+    if (
+        !isSplattered &&
+        mouseX >= bugX - 25 &&
+        mouseX <= bugX + bugSize + 25 &&
+        mouseY >= bugY - 25 &&
+        mouseY <= bugY + bugSize + 25
+    ) {
+        score++;
+        hoppingInterval -= 100;
+        isSplattered = true;
+        updateScoreDisplay();
 
-            // Draw the splattered bug image and reset it after a delay
-            drawBug();
-            setTimeout(() => {
-                isSplattered = false;
-                updateBugPosition();
-            }, 1000);
-        }
+        // Temporarily show the splattered bug image
+        drawBug();
+        setTimeout(() => {
+            isSplattered = false;
+            updateBugPosition();
+        }, 1000); // Set the time to show the splattered bug image before respawning (in milliseconds)
     }
+}
 
+// Reset the hopping interval to its initial value
 function resetSpeed() {
     hoppingInterval = 1000;
 }
@@ -94,16 +89,12 @@ function resetScore() {
 }
 
 // Update the game state and draw the bug
-    function updateGame() {
-        // Update the bug position
-        updateBugPosition();
-        // Draw the bug
-        drawBug();
-        // Update the score display
-        updateScoreDisplay();
-        // Call this function again after a delay
-        setTimeout(updateGame, hoppingInterval);
-    }
+function updateGame() {
+    updateBugPosition();
+    drawBug();
+    updateScoreDisplay();
+    setTimeout(updateGame, hoppingInterval);
+}
 
 // Event listeners
 canvas.addEventListener("mousedown", handleClick);
